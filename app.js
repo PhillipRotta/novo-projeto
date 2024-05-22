9e75bd76ba2f217ecbbf417fa284c1420547eff3
PhillipRotta <philliprotta@gmail.com>
2024-05-22 09:40:20 -0300

    nova aula numero secreto

---
+++ app.js
@@ -0,0 +1,26 @@
+function sortear(){
+    let quantidade = parseInt(document.getElementById('quantidade').value); //parseint = tipo inteiro e não mais String
+    let de = parseInt(document.getElementById('de').value);
+    let ate = parseInt(document.getElementById('ate').value);
+
+    let sorteados = [];
+    let numero;
+
+    for (let i = 0; i < quantidade; i++) {
+        numero = obterNumeroAleatorio(de, ate);
+
+        while (sorteados.includes(numero)){ // while = enquanto
+            numero = obterNumeroAleatorio(de, ate);
+        }
+        
+        sorteados.push(numero);
+    }
+
+
+    let resultado = document.getElementById('resultado');
+    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${sorteados} </label>`
+}
+
+function obterNumeroAleatorio(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
